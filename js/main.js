a3deaaeb398b186dbe124425f51777e502b62420
Aaron Gonzalez <Kabku43@gmail.com>
2017-08-15 19:21:17 -0400

    Making sidebar nav open

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
 function toggle() {
-  // if (sidebar.style.display == "none") {
-  //     sidebar.style.display = "block";
-  // } else {
-  //     sidebar.style.display = "none";
-  // }
+  isNavItemsDisplayed = !isNavItemsDisplayed;
 
-  if(sidebar.style.width != "0px"){
-    console.log("Sidebar is closing " + sidebar.style.width)
-    sidebar.style.width = '0';
-  }
-  else{
-    console.log("Sidebar is opening");
-    sidebar.style.width = '10%';
+  $(sidebar).toggleClass('expand')
+  _updateNavItems(isNavItemsDisplayed ? 500 : 1500);
+}
+
+function _updateNavItems(time){
+  for(var i = 0; i < navItems.length; i++){
+    $(navItems[i]).stop(true,true)
+    $(navItems[i]).fadeToggle(time)
+    // isNavItemsDisplayed ? $(navItems[i]).fadeToggle(2000) : $(navItems[i]).fadeOut(300)
   }
 }
 
 var burger = document.getElementById('ps-ham-btn');
-var sidebar = document.getElementById('ps-side-nav')
-
+var sidebar = document.getElementById('ps-side-nav');
+var navItems = document.getElementsByClassName("ps-sidebar-item");
+var isNavItemsDisplayed = false;
 burger.addEventListener('click', toggle);
+toggle();
+toggle();
