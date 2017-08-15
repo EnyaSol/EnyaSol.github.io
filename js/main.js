72999f16be9d5ee59fe6cbd8153a2dcaae012df3
Aaron Gonzalez <Kabku43@gmail.com>
2017-08-15 17:32:19 -0400

    Added sidebar nav

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
-function toggle(elem) {
-  console.log("Toggling active")
-   elem.classList.toggle("active");
+function toggle() {
+  // if (sidebar.style.display == "none") {
+  //     sidebar.style.display = "block";
+  // } else {
+  //     sidebar.style.display = "none";
+  // }
+
+  if(sidebar.style.width != "0px"){
+    console.log("Sidebar is closing " + sidebar.style.width)
+    sidebar.style.width = '0';
+  }
+  else{
+    console.log("Sidebar is opening");
+    sidebar.style.width = '10%';
+  }
 }
 
-var burger = document.getElementById('hamburger');
+var burger = document.getElementById('ps-ham-btn');
+var sidebar = document.getElementById('ps-side-nav')
 
-document.addEventListener('click', toggle(burger));
+burger.addEventListener('click', toggle);
