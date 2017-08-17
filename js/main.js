48725c00ecf9358de3ab7a1b26cc628501dd58cb
Aaron Gonzalez <Kabku43@gmail.com>
2017-08-16 23:03:02 -0400

    Making header's fade into view

--- js/main.js
+++ js/main.js
@@ -14,10 +14,22 @@ function _updateNavItems(time){
   }
 }
 
+var introHeader1 = $('#ps-intro-h1');
+var introHeader2 = $('#ps-intro-h3');
+
 var burger = document.getElementById('ps-ham-btn');
 var sidebar = document.getElementById('ps-side-nav');
 var navItems = document.getElementsByClassName("ps-sidebar-item");
 var isNavItemsDisplayed = false;
+
+
+$(introHeader1).fadeIn(1500);
+setTimeout(function(){
+  $(introHeader2).fadeIn(1500);
+}, 1000);
+
 burger.addEventListener('click', toggle);
-toggle();
-toggle();
+window.onload = function(){
+  toggle();
+  toggle();
+}
